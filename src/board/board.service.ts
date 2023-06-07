2fbbc6a835c35fd5a9969308cb5cd4537debe0d4
cxzaqq <roy1228@naver.com>
2023-06-07 23:14:58 +0900

    get board 리뷰 반영

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,20 +1,41 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
 import { BoardRepository } from './repository/board.repository';
 
 @Injectable()
 export class BoardService {
   private readonly logger = new Logger(BoardService.name);
-  constructor(private readonly repository: BoardRepository) {}
+  constructor(private readonly boardRepository: BoardRepository) {}
 
+  /**
+   *get all boards
+   */
   async getAll() {
-    const query = `select b.title, b.contents, b."userId", b.recommand, b."dateTime", b."boardCategoryId",
-    u.nickname, u.img
-    from board b, public.user u
-    order by "dateTime" desc`;
     try {
-      return await this.repository.query(query);
+      const board = await this.boardRepository
+        .createQueryBuilder('board')
+        .select([
+          'board.title',
+          'board.dateTime',
+          'board.isDeleted',
+          'board.isModified',
+          'board.recommand',
+          'board.boardCategoryId',
+          'user.nickname',
+        ])
+        .leftJoin('board.user', 'user')
+        .orderBy('board.dateTime', 'DESC')
+        .getRawMany();
+
+      return board;
     } catch (err) {
       this.logger.error('게시판 조회 중 에러 발생');
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '게시판 조회 중 에러 발생',
+        },
+        404,
+      );
     }
   }
 }

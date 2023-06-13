a22a34bee242be6fccb7250c99123393ec7faf56
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-06-13 16:15:52 +0900

    Add rate limit

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  
+  // rateLimit({
+  //   windowMs: 15 * 60 * 1000, // 15 minutes
+  //   max: 100, // limit each IP to 100 requests per windowMs
+  // }),
 
   app.use(helmet());
   app.use(helmet.contentSecurityPolicy());

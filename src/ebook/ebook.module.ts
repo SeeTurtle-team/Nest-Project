fb67a0e215e091d24e6c91c21186462d29a825fc
cxzaqq <roy1228@naver.com>
2023-08-16 19:57:53 +0900

    분리된 getToken 적용

--- src/ebook/ebook.module.ts
+++ src/ebook/ebook.module.ts
@@ -3,10 +3,11 @@ import { EbookController } from './ebook.controller';
 import { EbookService } from './ebook.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EbookEntity } from 'src/entities/ebook.entity';
+import { GetToken } from 'src/utils/GetToken';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EbookEntity])],
   controllers: [EbookController],
-  providers: [EbookService],
+  providers: [EbookService, GetToken],
 })
 export class EbookModule {}

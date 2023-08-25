ca835f0fc9d48c57c346d68321c5413574ad1cd9
cxzaqq <roy1228@naver.com>
2023-08-25 15:30:43 +0900

    해당 유저 시리즈 조회

--- src/ebook/ebook.module.ts
+++ src/ebook/ebook.module.ts
@@ -4,9 +4,10 @@ import { EbookService } from './ebook.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EbookEntity } from 'src/entities/ebook.entity';
 import { GetToken } from 'src/utils/GetToken';
+import { EbookSeriesEntity } from 'src/entities/ebookSeries.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EbookEntity])],
+  imports: [TypeOrmModule.forFeature([EbookEntity, EbookSeriesEntity])],
   controllers: [EbookController],
   providers: [EbookService, GetToken],
 })

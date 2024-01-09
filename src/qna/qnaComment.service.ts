68fa90c054f7f323c7e487cddceaaca88638206c
slugjeon <youngman0515@naver.com>
2024-01-10 00:15:18 +0900

    fix:qna단일게시글조회시 타게시글의 댓글조회되는 문제 수정

--- src/qna/qnaComment.service.ts
+++ src/qna/qnaComment.service.ts
@@ -60,6 +60,7 @@ export class QnaCommentService {
     },
       HttpStatus.INTERNAL_SERVER_ERROR)
   }
+  //boardId의 모든 댓글조회
   async getAllComment(boarId: number,
     pageRequest?: PageRequest): Promise<Object> {
     try {
@@ -72,7 +73,7 @@ export class QnaCommentService {
         pageSize = pageRequest.pageSize;
       }
       const [count,page] = await Promise.all([this.qnaService.countAll(boarId),this.qnaCommentRepository.query(
-        `select  id, title,username, "dateTime","issecret" from "qnaComment" as q where q."isDeleted"=false and q."ban"=false order by q."parentId" desc offset ${offset} limit ${limit}`)]);
+        `select  id, title,username, "dateTime","issecret" from "qnaComment" as q where q."qnaId"=${boarId} and q."isDeleted"=false and q."ban"=false order by q."parentId" desc offset ${offset} limit ${limit}`)]);
       const returnPage = new Page(count, pageSize, page);
       return { success: true, Page: returnPage };
     } catch (err) {
@@ -84,8 +85,8 @@ export class QnaCommentService {
       },
         HttpStatus.INTERNAL_SERVER_ERROR)
     }
-  }
-  async getOneComment(id: number, headers: Headers, page?): Promise<Object> {
+  }//단일 댓글조회
+  async getOneComment(id: number, headers: Headers): Promise<Object> {
     try {
       const isComment = true;
       const verified = await this.getToken.getToken(headers);
@@ -111,6 +112,7 @@ export class QnaCommentService {
         HttpStatus.INTERNAL_SERVER_ERROR)
     }
   }
+  //댓글수정전 가져오기
   async getCommentUpdate(id, headers): Promise<Object> {
     try {
       const update = await this.getOneComment(id, headers);
@@ -136,9 +138,9 @@ export class QnaCommentService {
     }
   }
   /**
-   *qna 수정하기
+   *qnaComment 수정하기
    *
-   *@param updateQnaDto,headers
+   *@param updateQnaCommentDto,headers
    *@return  { success};
    */
   async updateComment(updateQnaCommentDto, headers): Promise<Object> {
@@ -171,6 +173,7 @@ export class QnaCommentService {
   }
 
   /**
+   * updateComment서브루틴
    * @param updateQnaCommentDto
    * @returns {Success,status}
    */
